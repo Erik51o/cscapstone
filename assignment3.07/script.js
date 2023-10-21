032edcd43234de9c498009d91d9d6d10ed40c4be
Erik51o <146995066+Erik51o@users.noreply.github.com>
2023-10-20 23:02:13 -0700

    Update script.js

--- assignment3.07/script.js
+++ assignment3.07/script.js
@@ -2,3 +2,16 @@ var p = document.querySelector("#button");
 
 p.innerHTML = "submit";
 p.style.background
+p.style.color = "red";
+p.style.textAlign = "center";
+
+function changeBackground()
+{
+    this.style.background = "green";
+    if(p.style.background = "green"){
+       p.innerHTML = "congrats"
+       p.style.color = "white"
+    }
+}
+
+p.onclick = changeBackground;
